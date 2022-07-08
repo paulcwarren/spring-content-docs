b031dd68232a7b976de2309fdfe58c950933e857
Paul Warren <paul_c_warren@yahoo.com>
2022-07-07 21:44:34 -0700

    Avoid pre-commit hook nastiness

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -18,10 +18,12 @@ mkdir -p $tmpdir
 
 git clone https://github.com/paulcwarren/spring-content $tmpdir
 pushd $tmpdir
+  # hack to avoid duet-prepare-commit-msg is not a git command
+  rm .git/hooks/prepare-commit-msg
   git checkout gh-pages
   cp -R $pwd/site/* .
   git add .
-  git commit -m "Docs update"
+  git commit --no-verify -m "Docs update"
   git pull -r
   git push origin gh-pages 
 popd
